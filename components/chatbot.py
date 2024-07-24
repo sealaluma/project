adaa90567a5f722175c732596131cefea2139d7d
ttuba93 <123141637+ttuba93@users.noreply.github.com>
2024-07-24 12:10:04 +0500

    Update chatbot.py

--- components/chatbot.py
+++ components/chatbot.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -13,8 +14,44 @@ import random
 import time
 import json
 
+# api_key = st.secrets["OPENAI_API_KEY"]
+
 def app():
     #Create the Streamlit app interface
     st.title("Chat-BOT")
     st.subheader("Твой личный ассистент по построению бизнеса.")
+
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+        with open("data/bot.json", "r", errors='ignore') as f:
+            data = json.load(f)
+        st_lottie(data)
+
+
+    st.title("📎 Бот-консультант")
+    st.caption("💭 Задай вопросы по бизнесу")
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "Что ты хочешь узнать?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
+        # Добавляем контекст о городе Алматы к сообщению пользователя
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        try:
+            client = OpenAI(api_key=openai_api_key)  # Используем api_key, так как openai_api_key уже определен
+            # Создаем запрос с контекстом о городе Алматы
+            response = client.chat.completions.create(model="gpt-3.5-turbo-0125", messages=st.session_state.messages)
+            msg = response.choices[0].message.content
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)
+        except Exception as e:
+            st.error(f"OpenAI Error: {e}")
 
